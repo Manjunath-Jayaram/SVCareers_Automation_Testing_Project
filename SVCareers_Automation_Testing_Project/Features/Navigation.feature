df074a0fa47184ea6a5ed5fcb04a6a6da4dfd5e3
manjunath-jayaram <manjunath.jayaram@softvision.com>
2017-11-17 11:08:54 +0530

    Updating the code base

---
+++ SVCareers_Automation_Testing_Project/Features/Navigation.feature
@@ -0,0 +1,10 @@
+﻿Feature: Navigation
+	In order to browse to the site
+	As a user
+	I want to use the browser address bar
+
+@GoToCareersPage
+Scenario: Browse to SVCareers page
+	Given Browser is open
+	And Navigation address is entered in the browser address bar
+	Then On Enter i should land in the SVCareers login page
---
+++ SVCareers_Automation_Testing_Project/StepDefinitions/NavigationSteps.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SVCareers_Automation_Testing_Project.StepDefinitions
+{
+    [Binding]
+    public class NavigationSteps
+    {
+        private IWebDriver webDriver;
+
+        [BeforeScenario]
+        public void InitScenario()
+        {
+            webDriver = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void TearDownScenario()
+        {
+            webDriver.Dispose();
+        }
+
+        [Given(@"Browser is open")]
+        public void GivenBrowserIsOpen()
+        {
+            webDriver.Navigate().GoToUrl("http://spicareers.spi.com/spicareers/");
+        }
+        
+        [Given(@"Navigation address is entered in the browser address bar")]
+        public void GivenNavigationAddressIsEnteredInTheBrowserAddressBar()
+        {
+            //webDriver.FindElement(By.)
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"On Enter i should land in the SVCareers login page")]
+        public void ThenOnEnterIShouldLandInTheSVCareersLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
