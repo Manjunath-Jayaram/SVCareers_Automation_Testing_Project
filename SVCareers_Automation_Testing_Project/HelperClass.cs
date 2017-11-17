0d94824eb380ad8f1a16f49f08da52466d645495
Manjunath-Jayaram <Manjunath.Jayaram@softvision.com>
2017-11-17 10:28:27 +0530

    Adding the repo

---
+++ SVCareers_Automation_Testing_Project/HelperClass.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVCareers_Automation_Testing_Project
+{
+    public static class HelperClass
+    {
+        public static IWebDriver Driver { get; set; }
+
+        static IWebElement _page = null;
+        public static void WaitForPageLoad(IWebDriver driver, string element)
+        {
+            var wait = new WebDriverWait(HelperClass.Driver, TimeSpan.FromMinutes(5));
+            Func<IWebDriver, bool> waitForElement = web => driver.PageSource.Contains(element);
+            wait.Until(waitForElement);
+
+            //if (_page != null)
+            //{
+            //    var waitForCurrentPageToStale = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            //    waitForCurrentPageToStale.Until(ExpectedConditions.StalenessOf(_page));
+            //}
+            //var waitForDocumentReady = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            //waitForDocumentReady.Until((wdriver) => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
+
+            //_page = driver.FindElement(By.TagName("html"));
+        }
+    }
+}
---
+++ SVCareers_Automation_Testing_Project/StepDefinitions/NewJobRequestSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SVCareers_Automation_Testing_Project.StepDefinitions
+{
+    [Binding]
+    public class NewJobRequestSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
